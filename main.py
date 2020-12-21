6f206621177939ac41204a2a2ffe7bd3b1c25736
Alexey <gooobler@gmail.com>
2020-12-21 11:32:12 +0700

    add comments & error print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+# входные данные для обучения и тестирования
 inputs = np.array([[1, 0, 0],
                    [1, 1, 0],
                    [1, 0, 1],
@@ -16,7 +16,7 @@ inputs = np.array([[1, 0, 0],
                    [0, 0, 0]
                    ])
 
-
+# выходные данные для обучения и тестирования
 outputs = np.array([[0.1],
                     [0.1],
                     [0.1],
@@ -32,9 +32,11 @@ outputs = np.array([[0.1],
                     [0.2]
                     ])
 
+# класс нейронной сети
+
 
 class NeuralNetwork:
-
+    # конструктор
     def __init__(self, inputs, outputs, hidden_nodes=[100, 200, 3], learning_rate=0.5, epochs=30000):
         self.inputs = inputs
         self.outputs = outputs
@@ -47,11 +49,13 @@ class NeuralNetwork:
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.bias = 0.000
+    # функция активация
 
     def sigmoid(self, x, deriv=False):
         if deriv == True:
             return x * (1 - x)
         return 1 / (1 + np.exp(-x))
+    # получение выхода сети
 
     def feed_forward(self):
         self.hidden_act1 = self.sigmoid(
@@ -62,6 +66,7 @@ class NeuralNetwork:
             np.dot(self.hidden_act2, self.weights3))
         self.output_act = self.sigmoid(
             np.dot(self.hidden_act3, self.weights4))
+    # обратное распространение ошибки
 
     def backpropagation(self):
         self.error = self.outputs - self.output_act
@@ -83,6 +88,7 @@ class NeuralNetwork:
         weight_delta1 = error1 * self.sigmoid(self.hidden_act1, deriv=True)
         self.weights1 += np.dot(self.inputs.T, weight_delta1
                                 ) * self.learning_rate
+    # алгоритм обучения
 
     def train(self):
         for epoch in range(self.epochs):
@@ -90,6 +96,7 @@ class NeuralNetwork:
             self.backpropagation()
             self.error_history.append(np.average(np.abs(self.error)))
             self.epoch_list.append(epoch)
+    # получение предсказания
 
     def predict(self, new_input):
         return self.sigmoid(
@@ -99,15 +106,17 @@ class NeuralNetwork:
                         np.dot(new_input+self.bias, self.weights1)), self.weights2)), self.weights3)), self.weights4))
 
 
+# итоговое тестирование
 for i in range(len(inputs)):
     train_data = np.delete(inputs, i, 0)
     train_output = np.delete(outputs, i, 0)
     NN = NeuralNetwork(train_data, train_output, hidden_nodes=[
         137, 101, 3], learning_rate=0.09, epochs=25000)
     NN.train()
-    print(NN.predict(inputs[i]), " -- Correct answer: ", outputs[i])
+    print(NN.predict(inputs[i]), " -- Correct answer: ", outputs[i],
+          " -- Error: ", NN.error_history[-1])
 
-
+# отрисовка графика
 plt.figure(figsize=(15, 5))
 plt.plot(NN.epoch_list, NN.error_history)
 plt.xlabel('Epoch')
